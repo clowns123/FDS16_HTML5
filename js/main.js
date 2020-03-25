230001ac7d7b3058067618b3c38a6b5c2dc3aa5b
seulbinim <seulbinim@gmail.com>
2020-03-25 10:58:45 +0900

    공공지사항 및 자료실 디자인 완성

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 var menuItems = $('.menu-item');
 var menuLinks = $('.menu-link');
 var subLinks = $('.submenu a');
+var sections = $('.board section');
 var boardItems = $('.board li');
+var tab = $('.tab');
 
 subLinks.attr('class', 'icon-dot-circled');
 boardItems.attr('class', 'icon-dot-circled');
@@ -10,4 +12,10 @@ menuLinks.click(function(e) {
   e.preventDefault();
   menuItems.removeClass('menu-item-act');
   $(this).parent().addClass('menu-item-act');
+});
+
+tab.click(function(e) {
+  e.preventDefault();
+  sections.removeClass('board-act');
+  $(this).parent().parent().addClass('board-act');
 });
